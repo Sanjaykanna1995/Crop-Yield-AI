aff0aab5aaa3d9ded9586da9b460063094af29c9
Sanjaykanna1995 <sanjaykanna.kj@gmail.com>
2026-03-01 12:14:01 +0530

    CORS changed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://crop-yield-ai.vercel.app"
+    ],
     credentials: true,
   })
 );
