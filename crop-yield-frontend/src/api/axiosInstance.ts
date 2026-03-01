dbf92a0b4ff125eec2894e6b204bd18a8480005e
Sanjaykanna1995 <sanjaykanna.kj@gmail.com>
2026-03-01 11:29:38 +0530

    Confirgured Production API for vercel

--- crop-yield-frontend/src/api/axiosInstance.ts
+++ crop-yield-frontend/src/api/axiosInstance.ts
@@ -1,20 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-export default axiosInstance;
+export default axiosInstance; 
